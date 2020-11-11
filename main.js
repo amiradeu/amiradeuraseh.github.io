61e4ced95f8c7f859915e24f28e0513998bc3424
amiradeu <amiradeu.x@gmail.com>
2020-11-12 01:29:26 +0800

    Simplify nav-active javascript

--- main.js
+++ main.js
@@ -1,16 +1,8 @@
 const navSlide = () => {
     const navOverlay = document.querySelector('.nav-overlay');
-    const burger = document.querySelector('.nav-burger');
-    const name = document.querySelector('.nav-name').querySelector('a');
-    const nav = document.querySelector('.nav-links');
-    const navSoc = document.querySelector('.nav-socials');
 
     burger.addEventListener('click', () => {
         navOverlay.classList.toggle('nav-active');
-        // name.classList.toggle('nav-name-active');
-        // nav.classList.toggle('nav-active');
-        // navSoc.classList.toggle('nav-active');
-        burger.classList.toggle('toggle');
     })
 }
 
