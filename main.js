63551a0505bd5329ce43ec699e568c26a4b07771
amiradeu <amiradeu.x@gmail.com>
2020-11-12 01:27:20 +0800

    Complete navigation display toggle

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const navSlide = () => {
+    const navOverlay = document.querySelector('.nav-overlay');
     const burger = document.querySelector('.nav-burger');
+    const name = document.querySelector('.nav-name').querySelector('a');
     const nav = document.querySelector('.nav-links');
     const navSoc = document.querySelector('.nav-socials');
 
     burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
-        navSoc.classList.toggle('nav-active');
+        navOverlay.classList.toggle('nav-active');
+        // name.classList.toggle('nav-name-active');
+        // nav.classList.toggle('nav-active');
+        // navSoc.classList.toggle('nav-active');
         burger.classList.toggle('toggle');
     })
 }
