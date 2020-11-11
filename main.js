119989f9d305d0304f37038eba4ae91b03695bcd
amiradeu <amiradeu.x@gmail.com>
2020-11-12 00:33:05 +0800

    Toggle nav display and animate burger

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function toggleHamburger(x) {
-    x.classList.toggle("change");
+const navSlide = () => {
+    const burger = document.querySelector('.nav-burger');
+    const nav = document.querySelector('.nav-links');
+    const navSoc = document.querySelector('.nav-socials');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        navSoc.classList.toggle('nav-active');
+        burger.classList.toggle('toggle');
+    })
 }
+
+navSlide();
+
+// const app  = () => {
+//     navSlide();
+// }
