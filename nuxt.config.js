b74651d1f0633b6244d937771afbe593dc65d6da
amiradeu <amiradeu.x@gmail.com>
2021-05-16 20:11:43 +0800

    Change nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,11 +1,14 @@
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: true,
+  ssr: false,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
   router: {
     base: '/amiradeuraseh.github.io/'
+  },
+  generate: {
+    fallback: true
   },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
