0b82574767632088eae99c612afe3c85e42aa871
amiradeu <amiradeu.x@gmail.com>
2021-05-16 19:37:00 +0800

    Change nuxt config ssr to true

---
+++ dist/_nuxt/static/1621164957/about/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Famiradeuraseh.github.io\u002F_nuxt\u002Fstatic\u002F1621164957",layout:"default",error:a,serverRendered:true,routePath:"\u002Fabout",config:{_app:{basePath:"\u002Famiradeuraseh.github.io\u002F",assetsPath:"\u002Famiradeuraseh.github.io\u002F_nuxt\u002F",cdnURL:a}}}}(null));
