6fd0db4e62ffb9370e1634fa72bd7b0145db5003
amiradeu <amiradeu@users.noreply.github.com>
2021-05-24 02:49:21 +0000

    deploy: 9fc14cafb2cbf175439f3b131ff92b24b1e7e3e1

---
+++ _nuxt/8f10186.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{279:function(e,n,t){"use strict";t.r(n);var c={},r=t(18),component=Object(r.a)(c,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,"d5184d10",null);n.default=component.exports}}]);
