4045785d3f17d2431c50c2291c5435cb90ddba65
amiradeu <amiradeu.x@gmail.com>
2021-05-25 16:09:00 +0800

    Add main profile

--- components/Profile.vue
+++ components/Profile.vue
@@ -1,38 +1,94 @@
 <template>
-    <div class="profile_header">
-        <h1>Amira Deuraseh</h1>
-        <h2>Creative <span id="dev">Developer</span> based in Malaysia</h2>
-        <h4>Designer & Developer</h4>
-        <p>available for hire</p>
+    <div>
+        <section class="section__profile">
+            <div class="full_name">
+                <h1 class="first_name">Amira<radiant class="radiant" /></h1>
+                <h1 class="last_name">Deuraseh</h1>
+            </div>
+            <p class="job_title"><em>Creative</em> Developer</p>
+            <p class="location">Based in Malaysia</p>
+            <div class="job_status"><span>available for hire</span></div>
+        </section>
     </div>
 </template>
 
 <script>
+import radiant from "~/assets/radiant_circle.svg?inline"
     export default {
-        
+        components: {
+            radiant
+        }
     }
 </script>
 
 <style lang="scss" scoped>
-div {
-    display: grid;
-    justify-items: center;
+.section__profile {
+    width: 100%;
+}
+
+h1 {
+    font-family: 'Butler';
+    font-size: 16vw;
+    font-weight: 200;
+    font-style: normal;
+    text-transform: uppercase;
+}
+
+.full_name {
+    display: flex;
+    flex-direction: column;
+    padding: 0 4vw;
+
+    h1:last-child {
+        align-self: flex-end;
+    }
+}
+
+.first_name {
+    position: relative;
+
+    .radiant {
+        position: absolute;
+        content: "";
+        top: 25%;
+        left: 22%;
+        height: 16vw;
+        width: 16vw;
+        z-index: -1;
+    }
+}
+
+.job_title {
+    font-family: 'Fanwood Text';
+    font-size: 8vw;
     text-align: center;
 }
 
-img {
-    border-radius:50%;
-    height: 100px;
-    width: 100px;
+.location {
+    text-align: center;
 }
 
-h1, h2, h4 {
+.job_status {
+    font-size: 2vw;
+    font-family: 'Butler';
     text-transform: uppercase;
+    transform: translateX(70vw);
 
-    // #dev {
-    //     -webkit-text-fill-color: transparent; /* Will override color (regardless of order) */
-    //     -webkit-text-stroke-width: 1px;
-    //     -webkit-text-stroke-color: black;
-    // }
+    span {
+        position: relative;
+
+        &:after {
+            position: absolute;
+            top: -70%;
+            left: 0%;
+            height: 220%;
+            width: 100%;
+            content: "";
+            border: 1px solid var(--candy);
+            border-radius: 50%;
+            transform: rotate3d(1, 1, -1, 15deg);
+            z-index: -1;
+        }
+    }
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,25 @@
 <template>
   <div>
-        <!-- <Profile />
-        <Service /> -->
+        <Profile class="main_section"/>
+        <!-- <Service /> -->
         <Socials />
         <Skill />
         <Email />
         <Footer />
   </div>
 </template>
+<script>
+
+export default {
+}
+</script>
 <style lang="scss" scoped>
 div > *:not(footer) {
     margin: 0 0 24px 0;
+}
+
+.main_section {
+    padding-top: 12vh;
+    margin-bottom: 64px;
 }
 </style>
