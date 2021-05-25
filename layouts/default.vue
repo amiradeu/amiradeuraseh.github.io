33727e87e24b56b2e2217672de26e876355f9585
amiradeu <amiradeu.x@gmail.com>
2021-05-25 15:05:42 +0800

    Add custom fonts and global css

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,63 +27,6 @@ export default {
 </script>
 
 <style lang="scss">
-:root {
-    --grey: #383838;
-    --white: #F3F5F5;
-    --gold: #DCCAA9;
-    --yellow: #FFD074;
-    --pink: #F6D2DE;
-    --red: #B05C52;
-    --green: #7CAD7C;
-    --charcoal: #373633;
-    --ivory: #F2F0E7;
-    --candy: #FF2121;
-}
-
-html {
-    font-family:
-        'Source Sans Pro',
-        -apple-system,
-        BlinkMacSystemFont,
-        'Segoe UI',
-        Roboto,
-        'Helvetica Neue',
-        Arial,
-        sans-serif;
-    font-size: 16px;
-    word-spacing: 1px;
-    -ms-text-size-adjust: 100%;
-    -webkit-text-size-adjust: 100%;
-    -moz-osx-font-smoothing: grayscale;
-    -webkit-font-smoothing: antialiased;
-    box-sizing: border-box;
-    background: var(--ivory);
-    color: var(--charcoal);
-    scroll-behavior: smooth;
-    -ms-overflow-style: none;  /* Internet Explorer 10+ */
-    scrollbar-width: none;  /* Firefox */
-}
-
-html::-webkit-scrollbar { 
-    display: none;  /* Safari and Chrome */
-}
-
-*,
-*::before,
-*::after {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-}
-
-a {
-    color: var(--grey);
-}
-
-ul {
-    list-style: none;
-}
-
 .main__layout {
     min-height: 100vh;
 }
@@ -100,9 +43,7 @@ ul {
     display: flex;
     flex-direction: column;
     justify-content: space-between;
-    align-items: center;
     z-index: 1;
-    padding-top: 12vh;
 }
 
 .background {
@@ -123,13 +64,6 @@ ul {
 .navOpen {
     .nuxt {
         display: none;
-    }
-}
-
-// mobile
-@media screen and (max-width: 728px){
-    .nuxt {
-        padding-top: 16vh;
     }
 }
 </style>
